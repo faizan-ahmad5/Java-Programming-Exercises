c54fc4f3fd5a0cb0712e74aed1a5099b41d8eb5e
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:49:27 +0500

    Basic Conditional Logic (if-else)

---
+++ Task-12/src/Main.java
@@ -0,0 +1,16 @@
+//Exercise 12: Basic Conditional Logic (if-else)
+//12.	Task: Write an if-else statement that checks whether a number is greater than 10.
+// If it is, print “Number is greater than 10”; otherwise, print “Number is less than or equal to 10.”
+//o	Expected Output:
+//Number is [Condition Result]
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number <= 10) {
+            System.out.println("Number is less than or equal to 10.");
+        } else {
+            System.out.println("Number is greater than 10.");
+        }
+    }
+}
