83e6920a176ce5442e2ba310482447bd82f511c8
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:42:28 +0500

    Declaring a Class and Main Method

---
+++ Task-02/src/FirstProgram.java
@@ -0,0 +1,8 @@
+//Exercise 2: Declaring a Class and Main Method
+//2.	Task: Declare a new class named FirstClass and within that, declare the main method as discussed in the lecture.
+//o	Expected Output: No specific output required yet, but the code should compile without errors.
+
+public class FirstProgram {
+    public static void main(String[] args) {
+    }
+}
