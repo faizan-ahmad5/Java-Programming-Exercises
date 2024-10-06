a9548ecc721b7e82edb1aba94501169cc1c68123
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:48:44 +0500

    Working with Char and Boolean

---
+++ Task-11/src/Main.java
@@ -0,0 +1,18 @@
+//Exercise 11: Working with Char and Boolean
+//11.	Task: Declare a char variable and assign a Unicode character. Also, declare a boolean variable and assign it true.
+// Print both values.
+//o	Expected Output:
+//Char value: [Your Unicode Character]
+//Boolean value: true
+//Look online for the Unicode table. Try to assign different Unicode values to the char datatype and print the result.
+// Match the values for correct output.
+
+public class Main {
+    public static void main(String[] args) {
+        char charVar = '\u0046';
+        boolean boolVar = true;
+        System.out.println("Char value: " + charVar);
+        System.out.println("Boolean value: " + boolVar);
+    }
+}
+
