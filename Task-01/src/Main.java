245e0b0161a4464691f901d0dcae3636120a6640
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:39:28 +0500

    Setting Up Your First Java Program

---
+++ Task-01/src/Main.java
@@ -0,0 +1,11 @@
+//Exercise 1: Setting Up Your First Java Program
+//1.	Task: Create a new project in IntelliJ, and write a Java program that prints “Hello, World!”.
+//o	Hint: Create a new class and include the public static void main(String[] args) method.
+//o	Expected Output:
+//Hello, World!
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
