0c32f63a47f9a0d13ba0747ce01db3f3488f4ad6
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:50:27 +0500

    Using Ternary Operator

---
+++ Task-13/src/Main.java
@@ -0,0 +1,12 @@
+//Exercise 13: Using Ternary Operator
+//13.	Task: Use a ternary operator to check whether a boolean value isAvailable is true or false, and print the result.
+//o	Expected Output:
+//Availability: [true/false]
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isAvailable = false;
+        String availabilityStatus = isAvailable ? "true" : "false";
+        System.out.println("Availability: " + availabilityStatus);
+    }
+}
