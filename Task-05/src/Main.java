55badac158d4d45efc1731c4a6611aed1a7476ec
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:44:37 +0500

    Working with Primitive Data Types

---
+++ Task-05/src/Main.java
@@ -0,0 +1,35 @@
+//Exercise 5: Working with Primitive Data Types
+//5.	Task: Declare variables for different data types: byte, short, int, long, float, double, char, and boolean.
+// Assign values to each and print them.
+//o	Expected Output:
+//Byte value: [Your Byte Value]
+//Short value: [Your Short Value]
+//Int value: [Your Int Value]
+//Long value: [Your Long Value]
+//Float value: [Your Float Value]
+//Double value: [Your Double Value]
+//Char value: [Your Char Value]
+//Boolean value: [Your Boolean Value]
+
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteContainer = 102;
+        short shortContainer = 21;
+        int intContainer = 1;
+        long longContainer = 2423234293L;
+        float floatContainer = 21.2f;
+        double doubleContainer = 2.232;
+        char charContainer = 'f';
+        boolean booleanContainer = true;
+
+        System.out.println("Byte value: " + byteContainer);
+        System.out.println("Short value: " + shortContainer);
+        System.out.println("Int value: " + intContainer);
+        System.out.println("Long value: " + longContainer);
+        System.out.println("Float value: " + floatContainer);
+        System.out.println("Double value: " + doubleContainer);
+        System.out.println("Char value: " + charContainer);
+        System.out.println("Boolean value: " + booleanContainer);
+    }
+}
