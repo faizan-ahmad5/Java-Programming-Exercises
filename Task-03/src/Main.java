268539976008dd82f41cc1e9905815083d723c0d
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:43:36 +0500

    Understanding Code Blocks and Statements

---
+++ Task-03/src/Main.java
@@ -0,0 +1,11 @@
+//Exercise 3: Understanding Code Blocks and Statements
+//3.	Task: Within the main method, write a statement that outputs your name to the console using System.out.println().
+//o	Expected Output:
+//My name is [Your Name]
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Faizan Ahmad Khan");
+    }
+}
