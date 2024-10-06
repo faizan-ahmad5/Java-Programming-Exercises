4684687b9d4b5fee5ea09007581b060baafc41e3
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-06 22:44:02 +0500

    Declaring Variables and Printing Their Values

---
+++ Task-04/src/Main.java
@@ -0,0 +1,10 @@
+//Exercise 4: Declaring Variables and Printing Their Values
+//4.	Task: Declare an integer variable age and set its value to your age. Then, print it using System.out.println().
+//o	Expected Output:
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println(age);
+    }
+}
